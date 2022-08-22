7243183742dfff1fae91260a40574bbf686f1ab3
Mobashir Farhan <farhanmobashir007@gmail.com>
2022-08-22 22:13:03 +0530

    fix

--- utils/colors.js
+++ utils/colors.js
@@ -32,24 +32,3 @@ export const neutral = {
   500: "#141414",
   600: "#111111",
 };
-
-// client id : d025c4260667427a9046c420b0c6cf5f
-// client secret : 3dc374eec9d6408c8ed3d1b61bd71aec
-
-// code =
-//   AQCG1bpWIXa2JzYBvX1kGgLd5IlFVSJee9evW50oosUO6UbJW2qezXGaaa4QFzcguTVpB2hdQ1M5AK8ACrXihwIDD56o2cIpHCFDrc050m9oHi2beHV6u38AzVOwPzPF2fddjo1nn0BiSnfcPRJwkogjC_BthciQYjNSTPPATEUTSeba3mPf085j_dGTh9doKyFH6Js7YpkKezBMopHLoeWh2E -
-//   6;
-
-//  code=AQCG1bpWIXa2JzYBvX1kGgLd5IlFVSJee9evW50oosUO6UbJW2qezXGaaa4QFzcguTVpB2hdQ1M5AK8ACrXihwIDD56o2cIpHCFDrc050m9oHi2beHV6u38AzVOwPzPF2fddjo1nn0BiSnfcPRJwkogjC_BthciQYjNSTPPATEUTSeba3mPf085j_dGTh9doKyFH6Js7YpkKezBMopHLoeWh2E-6
-
-//  base 64 : ZDAyNWM0MjYwNjY3NDI3YTkwNDZjNDIwYjBjNmNmNWY6M2RjMzc0ZWVjOWQ2NDA4YzhlZDNkMWI2MWJkNzFhZWM=
-
-/**
- * curl -H "Authorization: Basic ZDAyNWM0MjYwNjY3NDI3YTkwNDZjNDIwYjBjNmNmNWY6M2RjMzc0ZWVjOWQ2NDA4YzhlZDNkMWI2MWJkNzFhZWM="
--d grant_type=authorization_code -d code=AQAOG3IkcZPVP8Vaff1b4WYp8wzfy79-P5N3J66YdBuGO9kZfK6SPVuY0YiTgKY9YHZi_gmnSKuh_e9deVnPYvyBg1Ef5jOyf48RIsDidrBnucLyR4UFzLLEga7o3xZRR1nqaEtCU9dmU7CFHFhq_OMN3YT0j2bFZ6vI8yIWrk8TjhyeeBTz_2Ex6OJL3KT595n9YfzT-Eedk20JzfKBeE61wlga
- -d redirect_uri=http%3A
-%2F%2Flocalhost:3000 https://accounts.spotify.com/api/token
-
-
-{"access_token":"BQB06w7ImLLSZEZYBG8bXvz41W7tZjSt1aIrbHY71ktwdfbrf-lkzQkPCYJmKzfjrJvaxr_7VhVnHIaDgWmeOUkWtVnxXtMg_ejwPg6VlSAYvELQy2V7U64OziazLOVV7QPZkqvk91C1ORiN-teWqSexTAH9TytDYim2xzJNdaW9BXPsgZsOT_TwWUXwCnsf0ciP7VYvxv9qaS2h","token_type":"Bearer","expires_in":3600,"refresh_token":"AQChHH-UYq06EtF1SI9JEnJd7eLPayu3dNzP_1qYoEqb7JKWFgDg0PQpc7kqGPRw-7CnptP8twtl_uS4j1SDzwkYIMMKj5Y9AORo_aAOwZ3woSLhfdVs1jSz2hYKN1L3vgA","scope":"user-read-currently-playing user-top-read"}
- */
